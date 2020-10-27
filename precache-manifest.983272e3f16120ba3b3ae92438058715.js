e18c551f5a67b471c10d6ba33ac6a26fbf93c781
jinkyolee <63771067+jinkyolee@users.noreply.github.com>
2020-10-27 14:48:43 +0900

    Updates

---
+++ precache-manifest.983272e3f16120ba3b3ae92438058715.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c39047b96bbb266d5ce69b8054a30d",
+    "url": "/hsok-admin/index.html"
+  },
+  {
+    "revision": "b59fc9ee32fca628b940",
+    "url": "/hsok-admin/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "794667841aa5d2bf2d82",
+    "url": "/hsok-admin/static/css/main.5b627dbc.chunk.css"
+  },
+  {
+    "revision": "b59fc9ee32fca628b940",
+    "url": "/hsok-admin/static/js/2.22537f6c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/hsok-admin/static/js/2.22537f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794667841aa5d2bf2d82",
+    "url": "/hsok-admin/static/js/main.ff609e1c.chunk.js"
+  },
+  {
+    "revision": "500c62b3890f2be6eb36",
+    "url": "/hsok-admin/static/js/runtime-main.0dc0a2fc.js"
+  },
+  {
+    "revision": "5d17370f0842cfd0cb89e5911d6988ec",
+    "url": "/hsok-admin/static/media/add-img-btn.5d17370f.png"
+  },
+  {
+    "revision": "f6b18fb5166390849e51b38c0e6ddc29",
+    "url": "/hsok-admin/static/media/hanbok1.f6b18fb5.jpeg"
+  },
+  {
+    "revision": "df74a5ecb12a502526cbf485c3d67b5b",
+    "url": "/hsok-admin/static/media/hanbok2.df74a5ec.jpeg"
+  },
+  {
+    "revision": "bb8a921e8182240f0d5a6416619fc44d",
+    "url": "/hsok-admin/static/media/hanbok3.bb8a921e.jpeg"
+  },
+  {
+    "revision": "1af4fd1317e8ef1a30ed307056455d02",
+    "url": "/hsok-admin/static/media/hanbok4.1af4fd13.jpeg"
+  },
+  {
+    "revision": "29259bfb42f498b5eeab26d7b67d2aec",
+    "url": "/hsok-admin/static/media/logo.29259bfb.png"
+  }
+]);
